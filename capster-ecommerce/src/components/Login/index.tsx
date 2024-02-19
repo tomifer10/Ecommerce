14dfbd763045e2d06d534d71bdfb35def9a7fde1
tomifer10 <fernandeztomasig@gmail.com>
2024-02-19 22:43:29 +0100

    Login form OK
    Login form ready.

--- capster-ecommerce/src/components/Login/index.tsx
+++ capster-ecommerce/src/components/Login/index.tsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import "./login.css";
 
 type Props = {};
 
 export default function Login({}: Props) {
+  const [isLogginClicked, setLogginClicked] = useState(false);
   return (
     <div className="log-container">
-      <button id="firstlog">LOG IN</button>
+      {isLogginClicked ? (
+        <>
+          <div>
+            <form className="user-input" action="">
+              <input type="email" placeholder="Email" required></input>
+              <input type="password" placeholder="Password" required></input>
+              <button type="submit" id="firstlog">
+                LOG IN
+              </button>
+            </form>
+          </div>
+        </>
+      ) : (
+        <button id="firstlog" onClick={() => setLogginClicked(true)}>
+          LOG IN
+        </button>
+      )}
     </div>
   );
 }
